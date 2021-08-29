4ee08c02f81b5f578f517baf91e86e5a5ca51326
JacobAlbus <70151620+JacobAlbus@users.noreply.github.com>
2021-08-29 14:58:20 -0500

    Initial Frontend Commit

--- backend/api/textGeneration.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import textGenerationCtrl from "./textGeneration.controller.js"
-const router = express.Router()
-
-router.route("/").get(textGenerationCtrl.apiGenerateLyrics)
-
-export default router
--- src/http-text-generation.js
+++ src/http-text-generation.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/text-generation",
+    baseURL: "http://iris-publica-backend-env.eba-m8xnkvz4.us-east-2.elasticbeanstalk.com/api/v1/text-generation",
     headers: {
         "Content-type": "application/json"
     }
