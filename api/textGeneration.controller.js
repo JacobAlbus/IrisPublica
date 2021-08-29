174a3f4b65e79d3c1b2bbac2a6fe9fdbc8b063e7
JacobAlbus <70151620+JacobAlbus@users.noreply.github.com>
2021-08-29 15:31:33 -0500

    Still Fixing PythonShell

--- api/textGeneration.controller.js
+++ api/textGeneration.controller.js
@@ -4,31 +4,28 @@ const controller = class textGenerationDAO {
 
   static apiGenerateLyrics(req, res) {
 
-    let options = {
-      pythonPath: 'python',
-      scriptPath: '/var/app/current/dao/',
-      args: [req.query.songLength, req.query.artist]
-    };
+    // let options = {
+    //   pythonPath: 'python',
+    //   scriptPath: '/var/app/current/dao/text-generation/',
+    //   args: [req.query.songLength, req.query.artist]
+    // };
     
-    try {
-      PythonShell.run('text-generation/cli.py', options, function (err, results) {
-        if (err) throw err;
-        console.log('results: %j', results);
-  
-        let response = {
-          lyrics: results[0],
-        }
-  
-        res.json(response);
-      });
-    } catch (error) {
-      let response = {
-        lyrics: "soundtrack to my life",
-      }
+    // PythonShell.run('cli.py', options, function (err, results) {
+    //   if (err) throw err;
+    //   console.log('results: %j', results);
 
-      res.json(response);
+    //   let response = {
+    //     lyrics: results[0],
+    //   }
+
+    //   res.json(response);
+    // });
+
+    let response = {
+      lyrics: "Soundtrack to my life",
     }
 
+    res.json(response);
   }
 
 }
