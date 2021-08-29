d961f73635716e4889400c04646ee98a4d39d4fc
JacobAlbus <70151620+JacobAlbus@users.noreply.github.com>
2021-08-29 15:16:43 -0500

    Still Fixing PythonShell

--- api/textGeneration.controller.js
+++ api/textGeneration.controller.js
@@ -5,11 +5,12 @@ const controller = class textGenerationDAO {
   static apiGenerateLyrics(req, res) {
 
     let options = {
+      pythonPath: 'python',
       args: [req.query.songLength, req.query.artist]
     };
     
     try {
-      PythonShell.run('var/app/current/dao/text-generation/cli.py', options, function (err, results) {
+      PythonShell.run('dao/text-generation/cli.py', options, function (err, results) {
         if (err) throw err;
         console.log('results: %j', results);
   
