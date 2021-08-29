19ac2995649c227b4df0f614f713c69a535c0ad3
JacobAlbus <70151620+JacobAlbus@users.noreply.github.com>
2021-08-29 11:38:39 -0500

    Backend Commit

--- frontend/src/services/text-generation-data-services.js
+++
@@ -1,9 +0,0 @@
-import http from "../http-text-generation";
-
-class TextGenerationDataServices {
-  getLyrics(length=50, artist="Kanye West") {
-    return http.get(`/?artist=${artist}&songLength=${length}`);
-  }
-}
-
-export default new TextGenerationDataServices();
