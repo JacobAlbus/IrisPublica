951dc6c02697f4c1c7b1c0a49356bc6bd4e5f650
Jacob Albus <jacobalbus20@gmail.com>
2022-03-17 15:14:26 -0400

    Add Heroku

--- src/http-text-generation.js
+++ src/http-text-generation.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://iris-publica-backend-env.eba-m8xnkvz4.us-east-2.elasticbeanstalk.com/api/v1/text-generation",
+    baseURL: "https://cors-everywhere-me.herokuapp.com/http://iris-publica-backend-env.eba-m8xnkvz4.us-east-2.elasticbeanstalk.com/api/v1/text-generation",
     headers: {
         "Content-type": "application/json"
     }
