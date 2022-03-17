846edaaae8a1736945ab4e5770a8db1b91d33d28
Jacob Albus <jacobalbus20@gmail.com>
2022-03-17 15:34:06 -0400

    Remove CORS

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express"
-import cors from "cors"
+// import cors from "cors"
 import textGeneration from "./api/textGeneration.route.js"
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/text-generation", textGeneration)
