ab84322ab7e5e0ede1c57ac3ec061137aa989256
Jacob Albus <jacobalbus20@gmail.com>
2022-12-11 23:07:34 -0600

    Add compiled JJBA link

--- src/components/articles/jjba-strategy-game.js
+++ src/components/articles/jjba-strategy-game.js
@@ -16,6 +16,12 @@ const jjbaStrategyGame = props => {
           play as your favorite characters in the most memorable fights. The full source code can be found  
           <a href="https://github.com/JacobAlbus/final-project-JacobAlbus"> here</a>.</p>
     </div>
+
+    <div class="row">
+      <p>An already compiled version can be downloaded from <a href="https://drive.google.com/file/d/1EicNKnTVTD-zQt5T5GYH9H3IDwwnmQU_/view?usp=share_link"> here</a>.
+          Simply unzip the folder then run the "RUN_ME.exe" file in the root directory.
+      </p>
+    </div>
   </div>
   );
 };
